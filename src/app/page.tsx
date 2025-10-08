caf2cf321712ba19b7185ae74f8abf0a4aff493f
yats1304 <chaubaly@gmail.com>
2025-10-08 16:09:17 +0530

    upcoming webinar section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import TestimonialCard from "@/components/TestimonialCard";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -9,7 +10,8 @@ export default function Home() {
       <HeroSection />
       <FeaturedCourses />
       <WhyChooseUs />
-      <TestimonialCard/>
+      <TestimonialCard />
+      <UpcomingWebinars />
     </main>
   );
 }
